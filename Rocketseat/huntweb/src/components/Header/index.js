263edd4ea753f1dd543dbbd136354cee7d69ca84
Andreza Moreira <htmlandreza@gmail.com>
2019-01-22 11:17:29 -0200

    Componente Header

---
+++ Rocketseat/huntweb/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// estilo
+import "./styles.css"
+
+// função: stateless component
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+); 
+
+export default Header;
